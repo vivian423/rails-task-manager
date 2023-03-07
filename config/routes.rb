31f210181fd4e4fd763ba000f8cac4ec6bf55503
Vivian Chan <vivianchan423@gmail.com>
2023-03-07 14:41:35 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/tasks", to: "tasks#index"
+
+  get "/tasks/new", to: "tasks#new", as: :new_task
+
+  get "/tasks/:id", to: "tasks#show", as: :task
+
+  post "/tasks", to: "tasks#create", as: :create_post
+
+  get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "/tasks/:id", to: "tasks#update"
+
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy_task
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_112333) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
